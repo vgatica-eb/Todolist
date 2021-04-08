55234b662f6eb2e75f79f4e874e19de3891216fc
vgatica-eb <vgatica@eventbrite.com>
2021-04-08 12:59:07 -0300

    bootstrap

--- todolist/settings.py
+++ todolist/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolist_app'
+    'todolist_app',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
